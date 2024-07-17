7655f04e8c6c66d5aa770cacba9b71e7a6cf0458
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-07-17 17:13:48 +0300

    complete assighnment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+// Your code goes herdocument.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function() {
+        console.log("The DOM has loaded");
+          const paragraph = document.getElementById("text");
+          paragraph.textContent = "This is really cool!";
+        });
